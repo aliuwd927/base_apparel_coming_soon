b3953d5003a7045e1a1fcf68a9075a6bed89acec
Andy Liu <aliu927@gmail.com>
2020-09-29 23:57:36 -0700

    Update CSS and HTML. Added logic to empty field

--- js/index.js
+++ js/index.js
@@ -3,3 +3,14 @@
     The email address is not formatted correctly
 */
 
+let submit = document.getElementById('submit');
+let email = document.getElementById('email');
+
+submit.addEventListener('click', function(){
+    //alert('a');    /*Test button on click to ensure stacking order is correct*/
+    if(email.value.length = ' '){
+        //alert('container is empty'); /* Test container field is empty */
+        document.getElementById('hiddenEx').style.visibility = 'visible';
+        document.getElementById('hiddenPls').style.visibility = 'visible';
+    }
+})
